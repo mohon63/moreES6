eb5041afe4255e3dd9f85115d4b8f91e8d926a2a
mohon63 <mohonhossen63@gmail.com>
2022-08-15 18:02:45 +0600

    destructuring.js

---
+++ destructuring.js
@@ -0,0 +1,15 @@
+const employee = {
+    ide: 'vs code',
+    designation: 'developer',
+    mechine: 'mac',
+    language: ['html', 'css', 'js'],
+    specification: {
+        height: 66,
+        weight: 67,
+        address: 'kumrakhali',
+        drink: 'water'
+    }
+};
+
+const { mechine, ide } = employee;
+const { height, address } = employee.specification;
