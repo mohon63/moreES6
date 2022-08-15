7058030ce81a580352403706a9bb5d9a47aeb6e6
mohon63 <mohonhossen63@gmail.com>
2022-08-15 15:35:47 +0600

    find

--- array-methods.js
+++ array-methods.js
@@ -1,5 +1,5 @@
 const products = [
-    { name: 'Laptop', price: 3200, brand: 'Lenovo', colour: 'Silver' },
+    { name: 'laptop', price: 3200, brand: 'Lenovo', colour: 'Silver' },
     { name: 'Phone', price: 7000, brand: 'iPhone', colour: 'Black' },
     { name: 'Watch', price: 3000, brand: 'Casio', colour: 'Gray' },
     { name: 'Camera', price: 10000, brand: 'Canon', colour: 'Yellow' }
@@ -16,4 +16,6 @@ products.forEach(product => console.log(product.colour));
 const cheap = products.filter(product => product.price <= 5000);
 // console.log(cheap)
 const specificName = products.filter(product => product.name.includes('n'))
-console.log(specificName)
+// console.log(specificName)
+const specificNameFirstOne = products.find(product => product.name.includes('l'))
+console.log(specificNameFirstOne)
