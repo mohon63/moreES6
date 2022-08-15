b40e5440b8cc5ac94eb02db6ea05dd0ed59c7dfd
mohon63 <mohonhossen63@gmail.com>
2022-08-15 15:28:24 +0600

    map, filter, forEach,find

--- array-methods.js
+++ array-methods.js
@@ -5,7 +5,15 @@ const products = [
     { name: 'Camera', price: 10000, brand: 'Canon', colour: 'Yellow' }
 ];
 
+// 1. map
 const brands = products.map(product => product.brand);
 // console.log(brands);
 
+// 2. forEach
 products.forEach(product => console.log(product.colour));
+
+// 3. filter
+const cheap = products.filter(product => product.price <= 5000);
+// console.log(cheap)
+const specificName = products.filter(product => product.name.includes('n'))
+console.log(specificName)
